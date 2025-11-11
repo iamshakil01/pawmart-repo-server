4fd4c4452a78decaaa4ed8375e5f5c7bf16a7a93
Your Name <you@example.com>
2025-11-12 00:07:38 +0600

    added data fetching apis

--- index.js
+++ index.js
@@ -1,52 +1,88 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//  MiddleWare
-
 app.use(cors());
 app.use(express.json());
 
-
 const uri = "mongodb+srv://pawmart-db:P9S5Z8pZcg7Okyg7@firstdb.2rqimp0.mongodb.net/?appName=FirstDB";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
+
+    const db = client.db('pawmart-db');
+    const petsCollection = db.collection('pets-supplies');
 
 
 
+    // Find
+    app.get("/pets-supplies", async (req, res) => {
+      try {
+        const result = await petsCollection.find().toArray();
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Failed to load home listings" });
+      }
+    });
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+
+    // Find Latest Limit Data
+    app.get("/pets-supplies/home", async (req, res) => {
+      try {
+        const result = await petsCollection.find().sort({ _id: -1 }).limit(6).toArray();
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Failed to load home listings" });
+      }
+    });
+
+
+    // Find A Single Data
+    app.get("/pets-supplies/:id", async (req, res) => {
+      try {
+        const { id } = req.params
+        const result = await petsCollection.findOne({ _id: new ObjectId(id) })
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Failed to load home listings" });
+      }
+    });
+
+
+    // POST A Single Data
+    app.post("/pets-supplies", async (req, res) => {
+      const data = req.body
+      console.log(data)
+      const result = await petsCollection.insertOne(data)
+      res.send(result);
+
+    });
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected Ping");
+  } catch (err) {
+    console.error("MongoDB connection failed", err);
+  }
 }
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('PawMart is running ')
+  res.send('PawMart is running');
 });
 
-
-
 app.listen(port, () => {
-    console.log(`PawMart are running succesfully on port${port}`)
-})
+  console.log(`PawMart is running successfully on port ${port}`);
+});
