8e0fda95a021546470f590f8f15eab2d71285b02
Your Name <you@example.com>
2025-11-12 08:03:45 +0600

    vercel json added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db('pawmart-db');
     const petsCollection = db.collection('pets-supplies');
@@ -72,7 +72,7 @@ async function run() {
     });
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Connected Ping");
   } catch (err) {
     console.error("MongoDB connection failed", err);
